91b15b80431037a77d616033aa603049cb14a9e3
Melissa Desktop VM <msant035@ucr.edu>
2017-11-21 20:08:56 -0800

    Fixed glLoadMatrix

--- cs130_assn/minigl.cpp
+++ cs130_assn/minigl.cpp
@@ -412,16 +412,15 @@ void mglLoadIdentity()
  */
 void mglLoadMatrix(const MGLfloat *matrix)
 {
+	mat4 newMatrix = {matrix[0],matrix[1],matrix[2],matrix[3],
+			  matrix[4],matrix[5],matrix[6],matrix[7],
+			  matrix[8],matrix[9],matrix[10],matrix[11],
+			  matrix[12],matrix[13],matrix[14],matrix[15]};
+
 	if(matmode) {
-		matrixStackProj.top() = {matrix[0],matrix[4],matrix[8],matrix[12],
-								  matrix[1],matrix[5],matrix[9],matrix[13],
-								  matrix[2],matrix[6],matrix[10],matrix[14],
-								  matrix[3],matrix[7],matrix[11],matrix[15]};
+		matrixStackProj.top() = newMatrix;
 	} else {
-		matrixStackModel.top() = {matrix[0],matrix[4],matrix[8],matrix[12],
-								  matrix[1],matrix[5],matrix[9],matrix[13],
-								  matrix[2],matrix[6],matrix[10],matrix[14],
-								  matrix[3],matrix[7],matrix[11],matrix[15]};
+		matrixStackModel.top() = newMatrix; 
 	}
 }
 
